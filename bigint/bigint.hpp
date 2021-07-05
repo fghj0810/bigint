a871011f1fc3b6f61604b25ff556c4f101ecdf3f
fghj0810 <fghj0810@gmail.com>
2021-07-05 15:36:29 +0800

    显示构造

--- bigint/bigint.hpp
+++ bigint/bigint.hpp
@@ -9,7 +9,7 @@ namespace bigint {
 	class BigInt final
 	{
 	public:
-		constexpr static size_t getElementLength() { return SIZE / 8 / sizeof(uint32_t); }
+		constexpr static size_t getElementCount() { return SIZE / 8 / sizeof(uint32_t); }
 	public:
 		BigInt() = default;
 		~BigInt() = default;
@@ -17,6 +17,7 @@ namespace bigint {
 		BigInt(BigInt&&) = default;
 		BigInt& operator=(const BigInt& other) = default;
 		BigInt& operator=(BigInt&& other) = default;
+		template <size_t OSIZE> explicit BigInt(const BigInt<OSIZE>&);
 
 	public:
 		BigInt(const uint32_t& value);
@@ -54,7 +55,7 @@ namespace bigint {
 		BigInt& operator%=(const BigInt& other);
 
 	private:
-		uint32_t b[getElementLength()] = { 0 };
+		uint32_t b[getElementCount()] = { 0 };
 	};
 	namespace utility {
 		static inline int getValidBits(uint32_t num)
@@ -92,15 +93,24 @@ namespace bigint {
 			return ret;
 		}
 	}
+	template<size_t SIZE>template<size_t OSIZE>
+	inline BigInt<SIZE>::BigInt(const BigInt<OSIZE>& value)
+	{
+		size_t al = getElementCount();
+		size_t vl = BigInt<OSIZE>::getElementCount();
+		size_t l = std::min(al, vl);
+		uint32_t* srcP = (uint32_t*)(&value);
+		std::memcpy(b + al - l, srcP + vl - l, l * 4);
+	}
 	template<size_t SIZE>
 	inline BigInt<SIZE>::BigInt(const uint32_t& value)
 	{
-		b[getElementLength() - 1] = value;
+		b[getElementCount() - 1] = value;
 	}
 	template<size_t SIZE>
 	inline BigInt<SIZE>::BigInt(uint8_t* buf, size_t num)
 	{
-		num = std::min(num, 8 * getElementLength());
+		num = std::min(num, 8 * getElementCount());
 		int bufIndex = 0;
 		if ((num & 0x03) != 0)
 		{
@@ -113,7 +123,7 @@ namespace bigint {
 			bufIndex++;
 			b[64 - num / 4 - 1] = n2 | n3 | n4;
 		}
-		for (size_t i = getElementLength() - num / 4; i < getElementLength(); i++)
+		for (size_t i = getElementCount() - num / 4; i < getElementCount(); i++)
 		{
 			uint32_t n1 = static_cast<uint32_t>(buf[bufIndex + 0]) << 24;
 			uint32_t n2 = static_cast<uint32_t>(buf[bufIndex + 1]) << 16;
@@ -127,7 +137,7 @@ namespace bigint {
 	inline void BigInt<SIZE>::getBytes(uint8_t* buf) const
 	{
 		int bufIndex = 0;
-		for (int i = 0; i < getElementLength(); i++)
+		for (int i = 0; i < getElementCount(); i++)
 		{
 			buf[bufIndex] = b[i] >> 24;
 			bufIndex++;
@@ -160,16 +170,16 @@ namespace bigint {
 			auto cmp = a <=> b;
 			if (cmp == 0)
 			{
-				q.b[getElementLength() - 1] = 0x01;
+				q.b[getElementCount() - 1] = 0x01;
 			}
 			else if (cmp > 0)
 			{
 				const size_t bitsNumPerEle = 8 * sizeof(uint32_t);
-				r.b[getElementLength() - 1] |= a.b[0] >> (bitsNumPerEle - 1);
+				r.b[getElementCount() - 1] |= a.b[0] >> (bitsNumPerEle - 1);
 				if (r >= b)
 				{
 					r -= b;
-					q.b[getElementLength() - 1] = 0x01;
+					q.b[getElementCount() - 1] = 0x01;
 				}
 				for (size_t i = 1; i < SIZE; i++)
 				{
@@ -178,11 +188,11 @@ namespace bigint {
 					uint32_t mask = static_cast<uint32_t>(1) << ((bitsNumPerEle - 1) - bitIndex);
 					r <<= 1;
 					q <<= 1;
-					r.b[getElementLength() - 1] |= (a.b[bIndex] & mask) >> ((bitsNumPerEle - 1) - bitIndex);
+					r.b[getElementCount() - 1] |= (a.b[bIndex] & mask) >> ((bitsNumPerEle - 1) - bitIndex);
 					if (r >= b)
 					{
 						r -= b;
-						q.b[getElementLength() - 1] |= 0x01;
+						q.b[getElementCount() - 1] |= 0x01;
 					}
 				}
 			}
@@ -196,7 +206,7 @@ namespace bigint {
 		r = BigInt<SIZE>();
 		if (b <= 1)
 		{
-			if (b.b[getElementLength() - 1] == 0)
+			if (b.b[getElementCount() - 1] == 0)
 			{
 				return false;
 			}
@@ -214,16 +224,16 @@ namespace bigint {
 			}
 			else if (cmp == 0)
 			{
-				q.b[getElementLength() - 1] = 0x01;
+				q.b[getElementCount() - 1] = 0x01;
 			}
 			else
 			{
 				const size_t bitsNumPerEle = 8 * sizeof(uint32_t);
-				r.b[getElementLength() - 1] |= a.b[0] >> (bitsNumPerEle - 1);
+				r.b[getElementCount() - 1] |= a.b[0] >> (bitsNumPerEle - 1);
 				if (r >= b)
 				{
 					r -= b;
-					q.b[getElementLength() - 1] = 0x01;
+					q.b[getElementCount() - 1] = 0x01;
 				}
 				for (size_t i = 1; i < SIZE; i++)
 				{
@@ -232,11 +242,11 @@ namespace bigint {
 					uint32_t mask = static_cast<uint32_t>(1) << ((bitsNumPerEle - 1) - bitIndex);
 					r <<= 1;
 					q <<= 1;
-					r.b[getElementLength() - 1] |= (a.b[bIndex] & mask) >> ((bitsNumPerEle - 1) - bitIndex);
+					r.b[getElementCount() - 1] |= (a.b[bIndex] & mask) >> ((bitsNumPerEle - 1) - bitIndex);
 					if (r >= b)
 					{
 						r -= b;
-						q.b[getElementLength() - 1] |= 0x01;
+						q.b[getElementCount() - 1] |= 0x01;
 					}
 				}
 			}
@@ -246,16 +256,16 @@ namespace bigint {
 	template<size_t SIZE>
 	inline std::strong_ordering BigInt<SIZE>::operator<=>(const uint32_t& other) const noexcept
 	{
-		for (int i = 0; i < getElementLength() - 1; i++)
+		for (int i = 0; i < getElementCount() - 1; i++)
 		{
 			if (b[i] != 0)
 			{
 				return std::strong_ordering::greater;
 			}
 		}
-		if (b[getElementLength() - 1] != other)
+		if (b[getElementCount() - 1] != other)
 		{
-			if (b[getElementLength() - 1] > other)
+			if (b[getElementCount() - 1] > other)
 			{
 				return std::strong_ordering::greater;
 			}
@@ -269,7 +279,7 @@ namespace bigint {
 	template<size_t SIZE>
 	inline std::strong_ordering BigInt<SIZE>::operator<=>(const BigInt<SIZE>& other) const noexcept
 	{
-		for (size_t i = 0; i < getElementLength(); i++)
+		for (size_t i = 0; i < getElementCount(); i++)
 		{
 			if (b[i] != other.b[i])
 			{
@@ -299,7 +309,7 @@ namespace bigint {
 		{
 			if (shiftnum >= SIZE)
 			{
-				std::memset(b, 0, getElementLength() * sizeof(uint32_t));
+				std::memset(b, 0, getElementCount() * sizeof(uint32_t));
 			}
 			else
 			{
@@ -307,7 +317,7 @@ namespace bigint {
 				size_t shiftIntBit = shiftnum % (8 * sizeof(uint32_t));
 				if (shiftIntBit == 0)
 				{
-					for (size_t i = getElementLength() - 1; i >= shiftInt; i--)
+					for (size_t i = getElementCount() - 1; i >= shiftInt; i--)
 					{
 						size_t curIndex = i - shiftInt;
 						b[i] = b[curIndex];
@@ -315,7 +325,7 @@ namespace bigint {
 				}
 				else
 				{
-					for (size_t i = getElementLength() - 1; i > shiftInt; i--)
+					for (size_t i = getElementCount() - 1; i > shiftInt; i--)
 					{
 						size_t curIndex = i - shiftInt;
 						uint32_t& cur = b[curIndex];
@@ -343,7 +353,7 @@ namespace bigint {
 		{
 			if (shiftnum >= SIZE)
 			{
-				std::memset(b, 0, getElementLength() * sizeof(uint32_t));
+				std::memset(b, 0, getElementCount() * sizeof(uint32_t));
 			}
 			else
 			{
@@ -351,7 +361,7 @@ namespace bigint {
 				size_t shiftIntBit = shiftnum % (8 * sizeof(uint32_t));
 				if (shiftIntBit == 0)
 				{
-					for (size_t i = 0; i < getElementLength() - shiftInt; i++)
+					for (size_t i = 0; i < getElementCount() - shiftInt; i++)
 					{
 						size_t curIndex = i + shiftInt;
 						b[i] = b[curIndex];
@@ -359,16 +369,16 @@ namespace bigint {
 				}
 				else
 				{
-					for (size_t i = 0; i < getElementLength() - shiftInt - 1; i++)
+					for (size_t i = 0; i < getElementCount() - shiftInt - 1; i++)
 					{
 						size_t curIndex = i + shiftInt;
 						uint32_t& cur = b[curIndex];
 						uint32_t& next = b[curIndex + 1];
 						b[i] = (cur << shiftIntBit) | (next >> (32 - shiftIntBit));
 					}
-					b[getElementLength() - shiftInt - 1] = b[getElementLength() - 1] << shiftIntBit;
+					b[getElementCount() - shiftInt - 1] = b[getElementCount() - 1] << shiftIntBit;
 				}
-				std::memset(b + getElementLength() - shiftInt, 0, shiftInt * sizeof(uint32_t));
+				std::memset(b + getElementCount() - shiftInt, 0, shiftInt * sizeof(uint32_t));
 			}
 		}
 		return *this;
@@ -386,7 +396,7 @@ namespace bigint {
 		if (other != 0)
 		{
 			uint32_t overflow = other;
-			for (size_t i = getElementLength() - 1; i > 0; i--)
+			for (size_t i = getElementCount() - 1; i > 0; i--)
 			{
 				uint64_t tmp = static_cast<uint64_t>(b[i]) + overflow;
 				b[i] = static_cast<uint32_t>(tmp);
@@ -407,7 +417,7 @@ namespace bigint {
 	inline BigInt<SIZE>& BigInt<SIZE>::operator+=(const BigInt<SIZE>& other)
 	{
 		uint32_t overflow = 0;
-		for (size_t i = getElementLength() - 1; i > 0; i--)
+		for (size_t i = getElementCount() - 1; i > 0; i--)
 		{
 			uint64_t tmp = static_cast<uint64_t>(b[i]) + other.b[i] + overflow;
 			b[i] = tmp;
@@ -428,12 +438,12 @@ namespace bigint {
 	{
 		if (other != 0)
 		{
-			uint64_t a = static_cast<uint64_t>(b[getElementLength() - 1]) + (static_cast<uint64_t>(~other) + 1);
-			b[getElementLength() - 1] = static_cast<uint32_t>(a);
+			uint64_t a = static_cast<uint64_t>(b[getElementCount() - 1]) + (static_cast<uint64_t>(~other) + 1);
+			b[getElementCount() - 1] = static_cast<uint32_t>(a);
 			uint32_t overflow = a >> 32;
-			if (getElementLength() > 1)
+			if (getElementCount() > 1)
 			{
-				for (size_t i = getElementLength() - 2; i > 0; i--)
+				for (size_t i = getElementCount() - 2; i > 0; i--)
 				{
 					uint64_t tmp = static_cast<uint64_t>(b[i]) + 0xFFFFFFFF + overflow;
 					b[i] = static_cast<uint32_t>(tmp);
@@ -455,7 +465,7 @@ namespace bigint {
 	inline BigInt<SIZE>& BigInt<SIZE>::operator-=(const BigInt<SIZE>& other)
 	{
 		uint32_t overflow = 1;
-		for (size_t i = getElementLength() - 1; i > 0; i--)
+		for (size_t i = getElementCount() - 1; i > 0; i--)
 		{
 			uint64_t tmp = static_cast<uint64_t>(b[i]) + (static_cast<uint64_t>(~other.b[i]) + overflow);
 			b[i] = static_cast<uint32_t>(tmp);
@@ -476,12 +486,12 @@ namespace bigint {
 	{
 		if (other == 0)
 		{
-			std::memset(b, 0, getElementLength() * sizeof(uint32_t));
+			std::memset(b, 0, getElementCount() * sizeof(uint32_t));
 		}
 		else if (other > 1)
 		{
 			uint32_t overflow = 0;
-			for (size_t i = getElementLength() - 1; i > 0; i--)
+			for (size_t i = getElementCount() - 1; i > 0; i--)
 			{
 				uint64_t tmp = static_cast<uint64_t>(b[i]) * other + overflow;
 				b[i] = tmp;
@@ -496,21 +506,21 @@ namespace bigint {
 	{
 		BigInt<SIZE> retValue(*this);
 		uint64_t overflow = 0;
-		for (size_t i = getElementLength() - 1; i > 0; i--)
+		for (size_t i = getElementCount() - 1; i > 0; i--)
 		{
 			retValue.b[i] = static_cast<uint32_t>(overflow);
 			overflow >>= 32;
-			for (int j = 0; j < getElementLength() - i; j++)
+			for (int j = 0; j < getElementCount() - i; j++)
 			{
-				uint64_t tmp = static_cast<uint64_t>(b[getElementLength() - 1 - j]) * other.b[i + j] + retValue.b[i];
+				uint64_t tmp = static_cast<uint64_t>(b[getElementCount() - 1 - j]) * other.b[i + j] + retValue.b[i];
 				retValue.b[i] = static_cast<uint32_t>(tmp);
 				overflow += (tmp >> 32);
 			}
 		}
 		retValue.b[0] = static_cast<uint32_t>(overflow);
-		for (size_t j = 0; j < getElementLength(); j++)
+		for (size_t j = 0; j < getElementCount(); j++)
 		{
-			retValue.b[0] = b[getElementLength() - 1 - j] * other.b[j] + retValue.b[0];
+			retValue.b[0] = b[getElementCount() - 1 - j] * other.b[j] + retValue.b[0];
 		}
 		return retValue;
 	}
@@ -518,7 +528,7 @@ namespace bigint {
 	inline BigInt<SIZE>& BigInt<SIZE>::operator*=(const BigInt<SIZE>& other)
 	{
 		BigInt<SIZE> tmp = *this * other;
-		std::memcpy(b, tmp.b, getElementLength());
+		std::memcpy(b, tmp.b, getElementCount());
 		return *this;
 	}
 	template<size_t SIZE>
